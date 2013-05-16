9a77810b9c5cb84fd72c582ba88896391ee7357c
Becky Lewis <becky.lewis@tangentlabs.co.uk>
2013-05-16 09:45:34 +0100

    added post and put

--- trafficlive/api.py
+++ trafficlive/api.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from trafficlive.connection import Connection
 
 
@@ -7,7 +9,7 @@ class StaffEmployee(object):
     def __init__(self, connection):
         self.conn = connection
 
-    def get_list(self, window_size=None, current_page=None, filter_by=None, order=None, data_format='json'):
+    def get_staff_list(self, window_size=None, current_page=None, filter_by=None, order=None, data_format='json'):
         params = {}
         if window_size:
             params['windowSize'] = window_size
@@ -18,7 +20,8 @@ class StaffEmployee(object):
         if order:
             params['order'] = order
 
-        staff_list = self.conn.get(self.url, params, headers={'Accept': 'application/json'})
+        staff_list = self.conn.get(self.url, params,
+                                   headers={'Accept': 'application/json'})
         return staff_list
 
     def get_by_id(self, pk, data_format='json'):
@@ -26,9 +29,14 @@ class StaffEmployee(object):
         staff_member = self.conn.get(url, headers={'Accept': 'application/json'})
         return staff_member
 
-    def post(self):
-        pass
+    def post(self, empolyee_data):
+        """ Posts the changes as json data
+        """
+        response = self.conn.post(self.url, body=simplejson.dumps(employee_data),
+                                  headers={'Accept': 'application/json', 'Content-type': 'appication/json'})
+        return response
 
-    def put(self):
-        pass
+    def put(self, employee_data):
+        response = self.conn.put(self.url, body=simplejson.dumps(employee_data),
+                                 headers={'Accept': 'application/json', 'Content-type': 'application/json'})
 
