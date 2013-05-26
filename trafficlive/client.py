b6f42a87422d53966c7f8770244287ed362855aa
Becky Lewis <becky.lewis@tangentlabs.co.uk>
2013-05-26 13:38:45 +0100

    Added config parser

--- trafficlive/client.py
+++ trafficlive/client.py
@@ -1,2 +1,20 @@
 """ Client module for the API wrapper.
+    The config parser code will eventually be moved out
+    of this module. It is only here so that this development
+    version of the API wrapper can be easily tested without having to
+    also create a test application
 """
+import ConfigParser
+
+
+class Client(object):
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+
+if __name__ == '__main__':
+    config = ConfigParser.ConfigParser()
+    config.readfp(open('traffic.conf'))
+    api_key = config.get('API', 'KEY')
+
+
