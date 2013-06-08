cd572fb73910aac6f18b5cadf8cf347bacd9dffb
Becky Lewis <github@scaryclam.co.uk>
2013-06-08 11:56:26 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='TrafficApi',
+      version='0.1',
+      description='Python Wrapper for traffics open api',
+      author='Becky Lewis',
+      author_email='trafficapi@scaryclam.co.uk',
+      url='https://github.com/scaryclam/traffic-api',
+      packages=['trafficapi'],
+)
+
